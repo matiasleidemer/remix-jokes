077d8d5764cfdfcb54de9dea8ce08a8c22bca0dd
Matias Leidemer <matiasleidemer@gmail.com>
2021-11-28 10:08:33 -0300

    Load joke in jokes/$jokeId path

--- app/routes/jokes/$jokeId.tsx
+++ app/routes/jokes/$jokeId.tsx
@@ -1,11 +1,33 @@
+import { useLoaderData, Link } from "remix";
+import { db } from "~/utils/db.server";
+
+import type { LoaderFunction } from "remix";
+import type { Joke } from "@prisma/client";
+
+type LoaderData = { joke: Pick<Joke, "content" | "name"> };
+
+export let loader: LoaderFunction = async ({ params }) => {
+  const { jokeId } = params;
+  const joke = await db.joke.findUnique({
+    where: { id: jokeId },
+    select: { content: true, name: true },
+  });
+
+  if (!joke) throw new Error("Joke not found!");
+
+  return { joke };
+};
+
 export default function JokeRoute() {
+  const { joke } = useLoaderData<LoaderData>();
+
   return (
     <div>
       <p>Here's your hilarious joke:</p>
       <p>
-        Why don't you find hippopotamuses hiding in trees? They're really good
-        at it.
+        <q>{joke.content}</q>
       </p>
+      <Link to=".">{joke.name} Permalink</Link>
     </div>
   );
 }
